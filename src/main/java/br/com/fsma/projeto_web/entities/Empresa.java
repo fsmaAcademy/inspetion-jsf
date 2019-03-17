87719160c3142e322cdcf2fc7a3dcd69b4b02739
Thiago Cunha <grupocodenome@gmail.com>
2019-03-17 15:56:38 -0300

    Modelando empresa

---
+++ src/main/java/br/com/fsma/projeto_web/entities/Empresa.java
@@ -0,0 +1,140 @@
+package br.com.fsma.projeto_web.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_empresa")
+public class Empresa implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Long id;
+
+	@Column(name = "cnpj", length = 18, nullable = false, unique = true)
+	private String cnpj;
+
+	@Column(name = "nome", length = 150, nullable = false, unique = true)
+	private String nome;
+
+	@Column(name = "logradouro", length = 150, nullable = false)
+	private String logradouro;
+
+	@Column(name = "cep", length = 9, nullable = false)
+	private String cep;
+
+	@ManyToOne
+	private Bairro bairro;
+
+	@ManyToOne
+	private Cidade cidade;
+
+	@ManyToOne
+	private Estado estado;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public Bairro getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(Bairro bairro) {
+		this.bairro = bairro;
+	}
+
+	public Cidade getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(Cidade cidade) {
+		this.cidade = cidade;
+	}
+
+	public Estado getEstado() {
+		return estado;
+	}
+
+	public void setEstado(Estado estado) {
+		this.estado = estado;
+	}
+
+	@Override
+	public String toString() {
+		return "Empresa [id=" + id + ", cnpj=" + cnpj + ", nome=" + nome + ", logradouro=" + logradouro + ", cep=" + cep
+				+ ", bairro=" + bairro + ", cidade=" + cidade + ", estado=" + estado + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Empresa other = (Empresa) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		return true;
+	}
+
+}
